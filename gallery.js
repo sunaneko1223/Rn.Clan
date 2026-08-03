0979f877f561e947fecd30ed3083ec5682e8f076
sunaneko1223 <sakacchi1223@gmail.com>
2026-08-03 10:49:03 +0900

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -17,6 +17,6 @@ const GALLERY_PLACEHOLDER_COUNT = 8;
 const GALLERY_IMAGES = [
   { src: "assets/gallery/拠点内.png", caption: "簡易拠点" },
   { src: "assets/gallery/拠点外.png", caption: "拠点周り" },
-  { src: "assets/gallery/採掘場", caption: "採掘場" },
+  { src: "assets/gallery/採掘場.png", caption: "採掘場" },
   { src: "assets/gallery/ねざー.png", caption: "ネザーポータル付近" },
 ];
