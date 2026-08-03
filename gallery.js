13a246026e3e06a93ac03f1a2332988b25639b90
sunaneko1223 <sakacchi1223@gmail.com>
2026-08-03 10:37:28 +0900

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -15,6 +15,8 @@
 const GALLERY_PLACEHOLDER_COUNT = 8;
 
 const GALLERY_IMAGES = [
-  // 例:
-  // { src: "assets/gallery/spawn-castle.png", caption: "スポーン城" },
+  { src: "assets/gallery/拠点内.png", caption: "簡易拠点" },
+  { src: "assets/gallery/拠点外.png", caption: "拠点周り" },
+  { src: "assets/gallery/採掘場", caption: "採掘場" },
+  { src: "assets/gallery/ねざー.png", caption: "ネザーポータル付近" },
 ];
